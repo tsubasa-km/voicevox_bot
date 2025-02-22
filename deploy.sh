8ca372f1343bce6b20c0434a68e073c0757e2629
tsubasa-km <mc0627tsubasa283@gmail.com>
2025-02-23 06:03:20 +0900

    スクリプト関連の追加

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+cd /home/tsubasa/git-repositories/voicevox_bot/
+
+forever stop index.js
+
+git pull
+
+npm install
+
+forever start index.js
+
---
+++ run_vvengine.sh
@@ -0,0 +1,5 @@
+cd /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable voicevox.service
+sudo systemctl start voicevox.service
+
